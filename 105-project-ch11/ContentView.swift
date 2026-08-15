248557e1601d3cd695004f5373c61047ec9b2854
brittanygrigsby <brittanysugrigsby@gmail.com>
2026-08-15 06:42:25 -0700

    Initial Commit

---
+++ 105-project-ch11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  105-project-ch11
+//
+//  Created by Brittany Grigsby on 8/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 105-project-ch11/_05_project_ch11App.swift
@@ -0,0 +1,17 @@
+//
+//  _05_project_ch11App.swift
+//  105-project-ch11
+//
+//  Created by Brittany Grigsby on 8/15/26.
+//
+
+import SwiftUI
+
+@main
+struct _05_project_ch11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
